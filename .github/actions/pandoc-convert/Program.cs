046b4e517f4678169c6c9881037e7464b838c095
Sidney Andrews <sidney@seesharprun.net>
2020-03-17 14:59:38 -0400

    Count markdown files

---
+++ .github/actions/pandoc-convert/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+using Console = Colorful.Console;
+
+namespace App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteAscii("Finding Markdown Files");
+            int count = 0;
+            foreach(string markdownFile in Directory.GetFiles(Directory.GetCurrentDirectory(), "**/*.md"))
+            {
+                Console.WriteLine($"[{++count}]\t{markdownFile}");
+            }
+        }
+    }
+}
